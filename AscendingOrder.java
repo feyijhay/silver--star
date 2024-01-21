32f75220829e2f86eb828d6e6178b8ddd30d1b63
feyijhay <josephfeyishetan@gmail.com>
2024-01-21 18:44:16 +0100

    assessment

---
+++ AscendingOrder.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class AscendingOrder{
+	public static void main(String[]args){
+	
+	int[] number = {50,10,100,60,90,70,20,80,40,30};
+
+
+Arrays.sort(number);
+
+System.out.println("The element in the array are:");
+
+
+
+	for(int check=0; check<number.length; check++){
+	System.out.println(number[check]);
+	}
+
+
+
+
+
+
+
+
+	}
+
+
+
+
+}
